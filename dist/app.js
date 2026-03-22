90a4fe4bd2d33eb06a19469c1cfd878a2f0143dc
ViktoriaRosovska <torimeister@gmail.com>
2026-03-22 12:08:58 +0200

    create project

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class App {
+    run() {
+        console.log("Done");
+    }
+}
+const app = new App();
+app.run();
+//# sourceMappingURL=app.js.map
